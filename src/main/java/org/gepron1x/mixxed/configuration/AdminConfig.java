8579b93c141851e9e825ff4b2141a2a8c0af7889
gepron1x <georgijpronuk@gmail.com>
2026-06-05 02:32:55 +0300

    add ability to add admins

---
+++ src/main/java/org/gepron1x/mixxed/configuration/AdminConfig.java
@@ -0,0 +1,34 @@
+package org.gepron1x.mixxed.configuration;
+
+
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import org.gepron1x.mixxed.entity.User;
+import org.gepron1x.mixxed.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminConfig {
+
+    @Value("${mixxed.admin.usernames}")
+    private String usernames;
+
+    private final UserRepository userRepository;
+
+    public AdminConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @PostConstruct
+    public void init() {
+        for(String username : usernames.split(",")) {
+            User user = userRepository.findByUsername(username).orElse(null);
+            if(user == null) continue;
+            if(user.isAdmin()) continue;
+            user.setAdmin(true);
+            userRepository.save(user);
+        }
+    }
+}
